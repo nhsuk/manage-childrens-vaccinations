5093d2bcdebc3aa99621f38de8c13931799278f4
Misha Gorodnitzky <misaka@dmfg.io>
2024-04-16 13:10:32 +0100

    Rename tabs initialiser to tab_paths

--- config/initializers/tab_paths.rb
+++ config/initializers/tab_paths.rb
@@ -11,5 +11,11 @@ TAB_PATHS = {
     "needed" => :needs_triage,
     "completed" => :triage_complete,
     "not-needed" => :no_triage_needed
+  },
+  vaccinations: {
+    "action-needed" => :action_needed,
+    "vaccinated" => :vaccinated,
+    "later" => :vaccinate_later,
+    "could-not" => :not_vaccinated
   }
 }.with_indifferent_access
